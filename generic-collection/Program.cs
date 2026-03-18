e6693504a3f11cb79dbdb596e1dcf07870f3e2ad
xuanfudev <xuanphu1024@gmail.com>
2026-03-18 12:37:51 +0700

    feat(bai9): unique element in list

--- generic-collection/Program.cs
+++ generic-collection/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello, World!");
+﻿List<int> list = new List<int>();
+List<int> newList = new List<int>();
+
+Console.Write("Nhap so luong phan tu: ");
+int n = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"Phan tu thu {i + 1}: ");
+    list.Add(int.Parse(Console.ReadLine()!));
+}
+
+foreach (var item in list)
+{
+    if (!newList.Contains(item))
+    {
+        newList.Add(item);
+    }
+}
+
+Console.Write("Danh sach khong trung: ");
+foreach (var item in newList)
+{
+    Console.Write(item + " ");
+}
