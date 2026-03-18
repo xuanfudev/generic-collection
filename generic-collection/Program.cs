2fdf7f594e47839fec672d7a6360fc030e49594d
xuanfudev <xuanphu1024@gmail.com>
2026-03-18 13:03:42 +0700

    feat(bai11): save list object

--- generic-collection/Program.cs
+++ generic-collection/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿List<Student> students = new List<Student>
+{
+    new Student { Id = 1, Name = "Alice" },
+    new Student { Id = 2, Name = "Bob" },
+    new Student { Id = 3, Name = "Charlie" }
+};
+
+foreach (var student in students)
+{
+    Console.WriteLine($"Id: {student.Id}, Name: {student.Name}");
+}
+
+class Student
+{
+    public int Id;
+    public required string Name;
+}
