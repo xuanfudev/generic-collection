a625a82ad00710c8c75371fa6ef235ac9dd1cfdd
xuanfudev <xuanphu1024@gmail.com>
2026-03-18 10:02:17 +0700

    feat(bai4): count even numbers

--- generic-collection/Program.cs
+++ generic-collection/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿List<int> list = new List<int>();
+
+Console.Write("Nhap do dai list: ");
+int lengthList = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < lengthList; i++)
+{
+    var number = Console.ReadLine();
+    list.Add(int.Parse(number!));
+}
+
+int evenCount = 0;
+
+foreach (var item in list)
+{
+    if (item%2 == 0)
+    {
+        evenCount++;
+    }
+}
+
+Console.WriteLine("So lương so chan trong list: " + evenCount);
