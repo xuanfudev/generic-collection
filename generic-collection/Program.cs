6865a62350fdc7d3db62793348a0b792efc9c878
xuanfudev <xuanphu1024@gmail.com>
2026-03-18 09:56:03 +0700

    feat(bai3): max in list

--- generic-collection/Program.cs
+++ generic-collection/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿List<int> list = new List<int>();
+
+Console.Write("Nhap do dai list: ");
+int lengthList = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < lengthList; i++)
+{
+    var number = Console.ReadLine();
+    list.Add(int.Parse(number!));
+}
+
+int max = list[0];
+
+foreach (var item in list)
+{
+    if (item > max)
+    {
+        max = item;
+    }
+}
+
+Console.WriteLine("Phan tu lon nhat trong list: " + max);
+Console.WriteLine("Phan tu lon nhat trong list: " + list.Max());
