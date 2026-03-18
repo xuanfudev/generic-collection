6cc7a563e384764f233abc388eef6436b10c7f45
xuanfudev <xuanphu1024@gmail.com>
2026-03-18 13:30:55 +0700

    feat(bai15): translate english to vietnames

--- generic-collection/Program.cs
+++ generic-collection/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Dictionary<string, string> dict = new Dictionary<string, string>();
+
+Console.Write("Nhap so luong tu: ");
+int n = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"Nhap tu tieng Anh {i + 1}: ");
+    string eng = Console.ReadLine()!;
+
+    Console.Write($"Nhap nghia tieng Viet {i + 1}: ");
+    string vie = Console.ReadLine()!;
+
+    dict[eng] = vie;
+}
+
+Console.Write("\nNhap tu tieng Anh can tra: ");
+string keyword = Console.ReadLine()!;
+
+if (dict.ContainsKey(keyword))
+{
+    Console.WriteLine($"Nghia: {dict[keyword]}");
+}
+else
+{
+    Console.WriteLine("Khong tim thay!");
+}
