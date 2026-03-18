45af3d2fb555685e5cdbab585e39e41ef14b97af
xuanfudev <xuanphu1024@gmail.com>
2026-03-18 10:42:29 +0700

    feat(bai6): find element in list

--- generic-collection/Program.cs
+++ generic-collection/Program.cs
@@ -1,1 +1,28 @@
-﻿Console.WriteLine("Hello, World!");
+﻿List<string> list = new List<string>();
+
+list.Add("Hello");
+list.Add("World");
+list.Add("Java");
+list.Add("C#");
+list.Add("Python");
+list.Add("C++");
+
+Console.Write("Nhap chuoi muon tim kiem: ");
+var findString = Console.ReadLine();
+
+bool found = false;
+
+foreach (var item in list)
+{
+    if (item.Equals(findString))
+    {
+        Console.WriteLine("Tim thay chuoi: " + item);
+        found = true;
+        break;
+    }
+}
+
+if (!found)
+{
+    Console.WriteLine("Khong tim thay chuoi!");
+}
