e00155f57abdd7ab0090c5f4c7e7ef2fe74a1bff
xuanfudev <xuanphu1024@gmail.com>
2026-03-18 12:40:34 +0700

    feat(bai10): find min in a list

--- generic-collection/Program.cs
+++ generic-collection/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿List<int> list = new List<int>();
+
+Console.Write("Nhap do dai list: ");
+int lengthList = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < lengthList; i++)
+{
+    var number = Console.ReadLine();
+    list.Add(int.Parse(number!));
+}
+
+int min = list[0];
+
+foreach (var item in list)
+{
+    if (item < min)
+    {
+        min = item;
+    }
+}
+
+Console.WriteLine("Phan tu nho nhat trong list: " + min);
+Console.WriteLine("Phan tu nho nhat trong list: " + list.Min());
