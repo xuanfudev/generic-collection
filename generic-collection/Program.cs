f6d9f635bcfe091f10f5bc538b2dfd79e257b494
xuanfudev <xuanphu1024@gmail.com>
2026-03-18 13:23:12 +0700

    feat(bai13): count student in a list

--- generic-collection/Program.cs
+++ generic-collection/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello, World!");
+﻿List<Student> students = new List<Student>();
+
+Console.Write("Nhap so luong sinh vien: ");
+int n = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < n; i++)
+{
+    Student sv = new Student();
+
+    Console.Write($"Nhap Id sinh vien {i + 1}: ");
+    sv.Id = int.Parse(Console.ReadLine()!);
+
+    Console.Write($"Nhap ten sinh vien {i + 1}: ");
+    sv.Name = Console.ReadLine()!;
+
+    students.Add(sv);
+}
+
+Console.WriteLine($"\nTong so sinh vien: {students.Count}");
+
+class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
