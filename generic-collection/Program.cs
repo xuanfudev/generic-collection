73af389cce70ac6bc43b2e615273fe807f0f482a
xuanfudev <xuanphu1024@gmail.com>
2026-03-18 10:34:08 +0700

    feat(bai5): remove element in list

--- generic-collection/Program.cs
+++ generic-collection/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello, World!");
+﻿List<string> list = new List<string>();
+
+Console.Write("Nhap do dai list: ");
+int lengthList = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < lengthList; i++)
+{
+    var student = Console.ReadLine();
+    list.Add(student!);
+}
+
+Console.WriteLine("Danh sach sinh vien: ");
+foreach (var student in list)
+{
+    Console.Write(student + ' ');
+}
+
+Console.Write("\n Ban muon xoa ten sinh vien nao: ");
+string studentRemove = Console.ReadLine()!;
+
+list.Remove(studentRemove);
+
+Console.WriteLine("Danh sach sinh vien sau khi xoa: ");
+foreach (var student in list)
+{
+    Console.Write(student + ' ');
+}
