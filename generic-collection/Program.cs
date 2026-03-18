66a5283fb89a29f20b170024e672972093c756de
xuanfudev <xuanphu1024@gmail.com>
2026-03-18 10:46:41 +0700

    feat(bai7): sort list

--- generic-collection/Program.cs
+++ generic-collection/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿List<string> list = new List<string>();
+
+list.Add("Hello");
+list.Add("World");
+list.Add("Java");
+list.Add("C#");
+list.Add("Python");
+list.Add("C++");
+
+Console.Write("Danh sach: ");
+foreach (var item in list)
+{
+    Console.Write(item + ' ');
+}
+
+list.Sort();
+
+Console.WriteLine("\nDanh sach sau khi sap xep: ");
+foreach (var item in list)
+{
+    Console.Write(item + ' ');
+}
