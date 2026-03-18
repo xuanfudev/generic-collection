c02f0c8a6aa22afcfe31664b6276f9108e7dcac6
xuanfudev <xuanphu1024@gmail.com>
2026-03-18 09:36:44 +0700

    feat(bai1): implement int list

--- generic-collection/Program.cs
+++ generic-collection/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿List<int> list = new List<int>();
+
+list.Add(1);
+list.Add(2);
+list.Add(3);
+list.Add(4);
+list.Add(5);
+
+Console.Write("List: ");
+foreach (var item in list)
+{
+    Console.Write(item + " ");
+}
