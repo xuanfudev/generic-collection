e2a6a5b4afc7c5c3be215dff10ed6432393cc832
xuanfudev <xuanphu1024@gmail.com>
2026-03-18 13:33:11 +0700

    feat(bai16): count word

--- generic-collection/Program.cs
+++ generic-collection/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Dictionary<char, int> dict = new Dictionary<char, int>();
+
+Console.Write("Nhap chuoi: ");
+string input = Console.ReadLine()!;
+
+foreach (char c in input)
+{
+    if (dict.ContainsKey(c))
+    {
+        dict[c]++;
+    }
+    else
+    {
+        dict[c] = 1;
+    }
+}
+
+Console.WriteLine("Ket qua:");
+foreach (var item in dict)
+{
+    Console.Write(item.Key + " : " + item.Value + " ");
+}
