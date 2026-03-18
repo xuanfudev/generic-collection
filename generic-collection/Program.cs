274ee55b5fcfef3a8cd20fd3579675c2c9254a8d
xuanfudev <xuanphu1024@gmail.com>
2026-03-18 12:33:35 +0700

    feat(bai8): reverse list

--- generic-collection/Program.cs
+++ generic-collection/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿List<int> list = new List<int>();
+
+list.AddRange(new[] { 1, 2, 3, 4, 5 });
+
+Console.Write("Danh sach ban dau: ");
+foreach (var item in list)
+{
+    Console.Write(item + " ");
+}
+
+list.Reverse();
+
+Console.Write("\nDanh sach sau khi dao nguoc: ");
+foreach (var item in list)
+{
+    Console.Write(item + " ");
+}
