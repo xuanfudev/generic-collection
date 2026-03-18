f3f988e2448da6f0d2dea5c3ba0d2f51c559dd38
xuanfudev <xuanphu1024@gmail.com>
2026-03-18 09:50:05 +0700

    feat(bai2): sum element in list

--- generic-collection/Program.cs
+++ generic-collection/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿List<int> list = new List<int>();
+
+Console.Write("Nhap do dai list: ");
+int lengthList = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < lengthList; i++)
+{
+    var number = Console.ReadLine();
+    list.Add(int.Parse(number!));
+}
+
+int sum = 0;
+
+foreach (var item in list)
+{
+    sum += item;
+}
+
+Console.WriteLine("Tong phan tu trong list: " + sum);
